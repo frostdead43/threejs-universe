b6549108e24bfcb149956872121cbab18edd7278
frostdead43 <frostdead@hotmail.com>
2025-11-09 17:31:42 +0300

    added glow and line effect

--- src/main.js
+++ src/main.js
@@ -69,7 +69,7 @@ return { scene, camera, render, controls };
 
   function createPlanets(scene) {
 
-
+      //Sun
       const sunGeometry = new THREE.SphereGeometry(15,32,32);
       const sunMaterial = new THREE.MeshStandardMaterial(
         {
@@ -83,6 +83,8 @@ return { scene, camera, render, controls };
       const sunMesh = new THREE.Mesh(sunGeometry,sunMaterial);
     scene.add(sunMesh);
 
+
+      //Alaz
       const alazGeometry = new THREE.SphereGeometry(3,64,64);
       const alazMaterial = new THREE.MeshStandardMaterial({
         color:"#8B0000",
@@ -96,9 +98,10 @@ return { scene, camera, render, controls };
     alazMesh.position.x = 30;
     scene.add(alazMesh);
     
+    //Mehmet & Linea
     const mehmetGeometry = new THREE.SphereGeometry(5,32,32);
     const mehmetMaterial = new THREE.MeshPhongMaterial( { 
-      color: "#aaccff",        // pastel mavi
+      color: "#aaccff",       
       roughness: 0.2,
       metalness: 0.1,
       transmission: 1,
@@ -110,24 +113,42 @@ return { scene, camera, render, controls };
       emissiveIntensity: 0.1,
     } );
 
-    //Mehmet & Linea
+    
     const mehmetMesh = new THREE.Mesh(mehmetGeometry,mehmetMaterial);
     mehmetMesh.position.x = 60;
     mehmetMesh.rotation.y = (Math.PI / 2)-135;
     scene.add(mehmetMesh);
 
+    const lineaGeometry = new THREE.SphereGeometry(5,32,32);
+    const lineaMaterial = new THREE.MeshStandardMaterial( { 
+        color: "#ff9966",        
+        roughness: 0.15,
+        metalness: 0.1,
+        transmission: 1,         
+        thickness: 1.3,
+        ior: 1.45,
+        clearcoat: 1,
+        clearcoatRoughness: 0.05,
+        // emissive: "#ff6600",    
+        // emissiveIntensity: 0.15,
+    } );
 
-    
+     const lineaMesh = new THREE.Mesh(lineaGeometry,lineaMaterial);
+    lineaMesh.position.x = 73;
+    lineaMesh.rotation.y = 400;
+    scene.add(lineaMesh);
+
+    // Gran Pulse
   const granPulseGeometry = new THREE.IcosahedronGeometry(15,1);
   const granPulseMaterial = new THREE.MeshPhysicalMaterial( { 
-    color: "#88e0ff",       // açık mavi ton    // yüzey pürüzsüz
-    metalness: 0.0,         // hafif metal yansıması
-    transmission: 0,        // saydamlık (1 = tamamen cam gibi)
-    thickness: 1.5,         // kalınlık, kırılmayı etkiler               // kırılma indeksi (elmas = 2.4, cam = 1.5)
+    color: "#88e0ff", 
+    metalness: 0.0,      
+    transmission: 0,       
+    thickness: 1.5,                
 
    } );
   const granPulseMesh = new THREE.Mesh( granPulseGeometry, granPulseMaterial );
-  granPulseMesh.position.x = 100;
+  granPulseMesh.position.x = 140;
   scene.add( granPulseMesh );
 
 
@@ -135,13 +156,73 @@ return { scene, camera, render, controls };
   const naiadGeometry = new THREE.SphereGeometry(9,32,32);
   const naiadMaterial = new THREE.MeshToonMaterial({color:"#00a2ff"});
   const naiadMesh = new THREE.Mesh(naiadGeometry,naiadMaterial);
-  naiadMesh.position.x = 150;
+  naiadMesh.position.x = 190;
   scene.add(naiadMesh);
 
 
+    const addWire = (mesh, color) => {
+    const edges = new THREE.EdgesGeometry(mesh.geometry);
+    const lineMaterial = new THREE.LineBasicMaterial({
+      color,
+      transparent: true,
+      opacity: 0.4
+    });
+    const wireframe = new THREE.LineSegments(edges, lineMaterial);
+    mesh.add(wireframe);
+  };
+  addWire(mehmetMesh, 0xffffff);
+  addWire(lineaMesh, 0xffffff);
 
+
+  // // Glow effect
+  // const glowMaterialA = new THREE.MeshBasicMaterial({
+  //   color: 0x99ccff,
+  //   transparent: true,
+  //   opacity: 0.15
+  // });
+  // const glowA = new THREE.Mesh(new THREE.SphereGeometry(6, 32, 32), glowMaterialA);
+  // glowA.position.copy(mehmetMesh.position);
+  // scene.add(glowA);
+
+  // const glowMaterialB = new THREE.MeshBasicMaterial({
+  //   color: 0xffbb88,
+  //   transparent: true,
+  //   opacity: 0.15
+  // });
+  // const glowB = new THREE.Mesh(new THREE.SphereGeometry(6, 32, 32), glowMaterialB);
+  // glowB.position.copy(lineaMesh.position);
+  // scene.add(glowB);
+
+
+  // Line
+  const lineGeometry = new THREE.BufferGeometry().setFromPoints([
+    mehmetMesh.position,
+    lineaMesh.position
+  ]);
+  const lineMaterial = new THREE.LineBasicMaterial({
+    color: "white",
+    linewidth: 10,
+    transparent: true,
+    opacity: 1 
+  });
+  const connectionLine = new THREE.Line(lineGeometry, lineMaterial);
+  scene.add(connectionLine);
 
     gsap.to(granPulseMesh.rotation, {
+      y: Math.PI * 10,
+      duration: 20,
+      repeat:-1,
+      ease: "none"
+    })
+
+    gsap.to(mehmetMesh.rotation, {
+      y: Math.PI * -10,
+      duration: 20,
+      repeat:-1,
+      ease: "none"
+    })
+
+    gsap.to(lineaMesh.rotation, {
       y: Math.PI * 10,
       duration: 20,
       repeat:-1,
